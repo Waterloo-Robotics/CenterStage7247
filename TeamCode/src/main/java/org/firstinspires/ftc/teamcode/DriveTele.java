373518482bde58844a3f45bee410e84b4b53b7f2
H2OLooBots <ftcteam7247@gmail.com>
2023-10-05 20:31:49 -0500

    05.10.2023 - Worked on adding lift mechanism

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTele.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.ftc.waterloo.h2oloobots.DriveTrain;
+import com.ftc.waterloo.h2oloobots.H2OLooOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Mecanum TeleOp", group = "!")
+public class DriveTele extends H2OLooOpMode {
+
+    @Override
+    public void opModeInit() {}
+
+    @Override
+    public void opModePeriodic() {
+
+        while (opModeIsActive()) {
+
+//            driveTrain.teleOpDrive(
+//                    gamepad1.left_stick_y,
+//                    gamepad1.left_stick_x,
+//                    gamepad1.right_stick_x
+//            );
+
+        }
+
+    }
+
+}
