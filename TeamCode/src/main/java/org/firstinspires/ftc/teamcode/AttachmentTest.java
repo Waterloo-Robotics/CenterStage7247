4c97cddbe576034e5012c4b8f0c8ce0a6c8e403e
H2OLooBots <ftcteam7247@gmail.com>
2023-10-05 20:31:49 -0500

    05.10.2023 - Worked on adding lift mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTest.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.ftc.waterloo.h2oloobots.DriveTrain;
-import com.ftc.waterloo.h2oloobots.H2OLooTeleOp;
+import com.ftc.waterloo.h2oloobots.H2OLooOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Attachment Test", group = "a")
-public class AttachmentTest extends H2OLooTeleOp {
+public class AttachmentTest extends H2OLooOpMode {
 
     @Override
     public void opModeInit() {}
@@ -14,15 +14,19 @@ public class AttachmentTest extends H2OLooTeleOp {
     @Override
     public void opModePeriodic() {
 
-            driveTrain.teleOpDrive(
-                    gamepad1.left_stick_y,
-                    gamepad1.left_stick_x,
-                    gamepad1.right_stick_x
-            );
+        while (opModeIsActive()) {
 
-            attachmentControl.liftTeleOp();
+//            driveTrain.teleOpDrive(
+//                    gamepad1.left_stick_y,
+//                    gamepad1.left_stick_x,
+//                    gamepad1.right_stick_x
+//            );
+
+            attachmentControl.liftTeleOp(gamepad1, gamepad2);
 
             telemetryControl.update();
+
+        }
 
     }
 
