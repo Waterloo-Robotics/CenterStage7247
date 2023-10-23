946e2cafc14fa5ce96ba28a4d25b935c1058c641
H2OLooBots <ftcteam7247@gmail.com>
2023-10-23 18:01:02 -0500

    Switched to OpenCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.ftc.waterloo.h2oloobots.Encoder;
 import com.ftc.waterloo.h2oloobots.H2OLooAuto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
@@ -14,10 +15,20 @@ public class Auto1 extends H2OLooAuto {
 
     public void opModePeriodic() {
 
-        while (opModeIsActive()) {
-            cameraControl.telemetryAprilTag();
-            telemetryControl.update();
-        }
+//        driveTrain.EncoderAutoMecanumDrive(4, 0, 0, 0.75, 3);
+//        sleep(500);
+//        driveTrain.EncoderAutoMecanumDrive(0, 0, -90, 0.75, 5);
+//        sleep(500);
+//        driveTrain.EncoderAutoMecanumDrive(72, 0, 0, 0.75, 8);
+
+//        odometryControl.strafeLeft(24, 0.5);
+
+//        while (opModeIsActive()) {
+//
+//            odometryControl.odoTelemetry(Encoder.MeasurementUnit.PULSES);
+//            cameraControl.telemetryAprilTag();
+//            telemetryControl.update();
+//        }
 
     }
 
