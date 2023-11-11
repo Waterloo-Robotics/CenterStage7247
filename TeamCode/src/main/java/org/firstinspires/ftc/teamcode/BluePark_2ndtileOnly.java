374437bb3ea77a8a557c8a176ef74a86966ca2f2
H2OLooBots <ftcteam7247@gmail.com>
2023-11-11 13:04:15 -0600

    Meet 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluePark_2ndtileOnly.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.ftc.waterloo.h2oloobots.H2OLooAuto;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class BluePark_2ndtileOnly extends H2OLooAuto {
+
+    public void opModeInit() {
+
+//        while (opModeIsActive()) cameraControl.telemetryAprilTag();
+
+    }
+
+    public void opModePeriodic() {
+
+        driveTrain.EncoderAutoMecanumDrive(24, 0, 0, 0.75, 3);
+        driveTrain.EncoderAutoMecanumDrive(0, 0, -90, 0.75, 5);
+        driveTrain.EncoderAutoMecanumDrive(84, 0, 0, 0.75, 8);
+        sleep(2000);
+        attachmentControl.intakeAuto();
+        driveTrain.EncoderAutoMecanumDrive(-6, 0, 0, 0.75, 1);
+        sleep(1000);
+        driveTrain.EncoderAutoMecanumDrive(6, 0, 0, 0.75, 1);
+        sleep(10000);
+
+    }
+
+}
